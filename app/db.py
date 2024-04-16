1c8560a8c07a9a0d827dc00f871758c008bcbd46
Adam Piątek <adam@MacBook-Air-Adam.local>
2024-04-16 12:36:27 +0200

    orm

--- app/db.py
+++ app/db.py
@@ -1,34 +1,42 @@
 import sqlite3
 import random
 from app.product.models import ProductModel
+from flask import jsonify
 
-def getNewId():
-    return random.getrandbits(28)
+# def getNewId():
+#     return random.getrandbits(28)
+
+def commit_and_close_connection(conn):
+    conn.commit()
+    conn.close() 
+
+def connect_to_database(db_path):
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    return conn, cur 
 
 def connect():
-    conn = sqlite3.connect('products.db')
-    cur = conn.cursor()
+    conn, cur = connect_to_database('const/products.db')
     cur.execute("""CREATE TABLE IF NOT EXISTS products 
-                (id INTEGER PRIMARY KEY,
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
                 product_name TEXT,
                 category TEXT,
                 producer TEXT,
                 description TEXT,
                 price FLOAT)""")
     
-    # Dodaj kolumnę 'producer', jeśli jeszcze jej nie ma
     try:
         cur.execute("ALTER TABLE products ADD COLUMN producer TEXT")
     except sqlite3.OperationalError as e:
-        # Jeśli kolumna już istnieje, zignoruj błąd
-        pass
+        return jsonify({
+            'res': "Can't create database",
+            'status': '500'
+         })
     
-    conn.commit()
-    conn.close()
+    commit_and_close_connection(conn)
 
 def insert(product):
-    conn = sqlite3.connect('products.db')
-    cur = conn.cursor()
+    conn, cur = connect_to_database('const/products.db')
     cur.execute("INSERT INTO products VALUES (?, ?, ?, ?, ?, ?)", (
         product.id,
         product.product_name,
@@ -37,12 +45,10 @@ def insert(product):
         product.description,
         product.price
     ))
-    conn.commit()
-    conn.close()
+    commit_and_close_connection(conn)
 
 def view():
-    conn = sqlite3.connect('products.db')
-    cur = conn.cursor()
+    conn, cur = connect_to_database('const/products.db')
     cur.execute("SELECT * FROM products")
     rows = cur.fetchall()
     products = []
@@ -52,9 +58,12 @@ def view():
     conn.close()
     return products
 
+def get_product(id):
+    conn, cur = connect_to_database('const/products.db')
+    cur.execute("SELECT * FROM products where id=?", id)
+
 def update(product):
-    conn = sqlite3.connect('products.db')
-    cur = conn.cursor()
+    conn, cur = connect_to_database('const/products.db')
     cur.execute("UPDATE products SET product_name=?, category=?, producer=?, description=?, price=? WHERE id=?", (
         product.product_name,
         product.category,
@@ -63,19 +72,14 @@ def update(product):
         product.price,
         product.id
     ))
-    conn.commit()
-    conn.close()
+    commit_and_close_connection(conn)
 
 def delete(id):
-    conn = sqlite3.connect('products.db')
-    cur = conn.cursor()
+    conn, cur = connect_to_database('const/products.db')
     cur.execute("DELETE FROM products WHERE id=?", (id, ))
-    conn.commit()
-    conn.close()
+    commit_and_close_connection(conn)
 
 def deleteAll():
-    conn = sqlite3.connect('products.db')
-    cur = conn.cursor()
+    conn, cur = connect_to_database('const/products.db')
     cur.execute("DELETE FROM products")
-    conn.commit()
-    conn.close()
+    commit_and_close_connection(conn)
